d21df30d6cae9b8be83143da1c90ab50a978bcce
Mike Merin <mikemerinweather@gmail.com>
2019-03-12 16:49:58 -0400

    game works!

--- javascript/game_handler.js
+++ javascript/game_handler.js
@@ -26,7 +26,7 @@ class Game_handler {
 	end_game(winner = "") {
 		$(".playboard").each((i, cell) => $(cell).unbind() );
 		this.game_status = winner ? winner + " has won!" : "Tie game!";
-		$("#game_status")[0].innerText = this.game_status;		
+		$("#instructions")[0].innerText = $("#game_status")[0].innerText = this.game_status;		
 	}
 	
 	attach_listeners() {
@@ -37,8 +37,7 @@ class Game_handler {
 					col = col.charCodeAt() - 65;
 					row = parseInt(row, 10) - 1;
 					
-					var player = this.player_turn();
-					this.board.board[row][col].value = e.target.innerText = player;
+					this.board.board[row][col].value = e.target.innerText = this.turn;
 					this.board.set_board_array();
 					
 					var winner = this.rules.check_if_winner(this.board.board_array);
@@ -46,6 +45,8 @@ class Game_handler {
 						this.end_game(winner);
 					} else if ( this.rules.check_if_tied(this.board.board_array) ) {
 						this.end_game();
+					} else {
+						this.move_made();
 					}
 				} else {
 //					$("#instructions")[0].innerText = ("invalid move");					
@@ -54,10 +55,9 @@ class Game_handler {
 		})
 	}
 	
-	player_turn() {
-		var player = "X"
-		$("#instructions")[0].innerText = "Player " + player + "'s Turn";
-		return player;
+	move_made() {
+		this.turn = (this.turn === "X" ? "O" : "X"); 
+		$("#instructions")[0].innerText = "Player " + this.turn + "'s Turn";
 	}
 
 	
