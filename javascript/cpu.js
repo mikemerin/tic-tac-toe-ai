76aef7b1c245dcff8b46e5abadc3173e86368b3d
Mike Merin <mikemerinweather@gmail.com>
2019-03-17 21:50:58 -0400

    beginnings of a CPU

--- javascript/cpu.js
+++ javascript/cpu.js
@@ -1,7 +1,32 @@
 class CPU {
 	
 	constructor() {
-		this.possible_moves = [];
+		this.possible_moves = {};
 	}
+	
+	get_possible_moves() {
+		page.board.board.forEach(row => {
+			row.forEach(cell => {
+		        if (cell.value === "") { this.possible_moves[cell.id] = 0 };
+		    })
+		})
+	}
+	
+	test_possible_moves() {
+		var temp_board = page.board.board;
+		Object.keys(this.possible_moves).forEach(cell => {
+			debugger
+		})
+		debugger
+	}
+	
+	set_board_array() {
+		this.board_array = this.board.map(row => {
+			return row.map(cell => {
+				return cell.value
+		    })
+		})
+	}
+	
 	
 }
